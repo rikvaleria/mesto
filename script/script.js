163d6db5b5c48815f639a531acc5856f446bf9b4
rikvaleria <71017691+rikvaleria@users.noreply.github.com>
2020-12-04 17:14:55 +0300

    review 1

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 let popup = document.querySelector('.popup');
 let buttonEdit = document.querySelector('.profile__btn-edit');
 let buttonClose = document.querySelector('.button_type_close');
+let buttonSave = document.querySelector('.button_type_save');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let openForm = document.querySelector('.popup__form');
@@ -9,9 +10,8 @@ let subtitleField = document.querySelector('.popup__input_type_job');
 
 function openPopup() {
     popup.classList.add('popup_opened');
-    event.preventDefault();
     profileTitle.textContent = titleField.value;
-    profileSubtitle.textContent =subtitleField.value;
+    profileSubtitle.textContent = subtitleField.value;
 }
 
 function closePopup() {
@@ -27,3 +27,4 @@ function submitForm(event) {
 buttonEdit.addEventListener('click', openPopup);
 buttonClose.addEventListener('click', closePopup);
 openForm.addEventListener('click', submitForm);
+buttonSave.addEventListener('click', closePopup);
