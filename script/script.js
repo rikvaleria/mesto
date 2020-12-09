e72a30696e131f54eb90e4fbfc03ae31023f5714
rikvaleria <71017691+rikvaleria@users.noreply.github.com>
2020-12-09 11:45:07 +0300

    review 3: fix

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-let popup = document.querySelector('.popup'); /*он открывает верстку попапа, если его удалить все ломается*/
+let popup = document.querySelector('.popup');
 let buttonEdit = document.querySelector('.profile__btn-edit');
 let buttonClose = document.querySelector('.popup__close');
 let buttonSave = document.querySelector('.popup__save');
